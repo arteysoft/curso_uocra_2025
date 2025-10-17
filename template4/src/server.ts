03ff57211f1b5d85d2b670df629c8bb64e566abb
andyLaptop <laptop@handys.com>
2025-10-16 22:45:07 -0300

    agrego fin de clase

--- template4/src/server.ts
+++ template4/src/server.ts
@@ -15,16 +15,17 @@ export let iniciarServidor = () => {
     app.get("/datos", async (req, res) => {
 
         let arrDatos:Array<any> = [
-            ['Fecha', 'Cotizacion']
+            ['Fecha', 'Cotizacion', 'media']
         ]
 
         const rutaArchivo = path.join(__dirname, '../../datos_mercado_libre.txt');
         let data = await fs.readFile(rutaArchivo, 'utf8')
         
-        const lineas = data.trim().split('\r\n');
+        const lineas = data.trim().split('\r\n').reverse();
         for (let linea of lineas.map(linea => linea.split('\t'))) {
-            arrDatos.push([linea[0], parseInt(linea[1])])
+            arrDatos.push([linea[0], parseInt(linea[1]), parseInt(linea[1])+10000])
         }
+        
         let objDatos = {arrDatos:arrDatos}
 
         res.send(JSON.stringify(objDatos)).end();
